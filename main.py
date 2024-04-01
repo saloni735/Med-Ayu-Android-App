27d2860e6cc15d049f719f814576f7e958213966
OIBSIP <117305867+saloni735@users.noreply.github.com>
2024-04-01 11:58:08 +0530

    Add files via upload
    MedAyu is an Android application that consists of multiple modules, including:
    
    Ayurvedic plant detection based on leafs:
    
    It uses a deep learning CNN model to predict the plant name based on leaf images.
    Disease prediction based on user symptoms:
    
    This module takes user symptoms as input and predicts diseases using machine learning models.
    It provides information about the disease, home remedies, and details about plants that can be beneficial.
    The application utilizes multiple APIs returned in Flask, and it also uses Firebase Realtime Database for storing data.

---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, jsonify
+from keras.models import load_model
+import tensorflow as tf
+import numpy as np
+
+# import cv2
+# import keras.applications.resnet50 import ResNet50
+# for importing image from url
+from skimage import io
+# from app import app
+
+app = Flask(__name__)
+
+model = load_model('model.h5')
+#this for pushing
+
+# model = ResNet50(weigths='imagenet')
+# model.save('')
+
+@app.route('/')
+def index():
+    return "Mini project MedAyu. Use /class with POST method to access  this API with params:test_url "
+
+
+@app.route('/class', methods=['POST'])
+def classify():
+   
+
+
+    with open("plants_name_114.txt","r") as file:
+        lines = file.readlines()
+    label_map ={}
+    for i in range(len(lines)):
+        label_map[i] = lines[i].strip()
+    print(label_map)
+    image_url = request.form.get('test_url')
+    image_array = io.imread(image_url)
+    # testing for preprocessing
+    img_array = np.rint(image_array)
+    img_array = img_array.astype('uint8')
+    # hsv_img = cv2.cvtColor(img_array, cv2.COLOR_RGB2HSV)
+    # mask = cv2.inRange(hsv_img, (25, 25, 0), (95, 270, 255))
+    # result = cv2.bitwise_and(img_array, img_array, mask=mask)
+    # result = result.astype('float64')
+
+    image_array = tf.image.resize(img_array, [150, 150])
+    img_array = tf.expand_dims(image_array, 0)
+
+    preds = model.predict(img_array)
+    # labling based on predication
+    print(preds)
+    predicted_class_indices = np.argmax(preds, axis=1)
+    prediction_labels = [label_map[k] for k in predicted_class_indices]
+    print(prediction_labels[0])
+    result = {'class': prediction_labels[0]}
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    print("working")
+    app.run(debug=True, host="0.0.0.0")
